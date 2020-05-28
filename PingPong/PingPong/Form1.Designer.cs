0c761491c3f844c5df195da7862113c07da6d2fd
MarkOstrotsky <MarkOstrotsky@github.com>
2020-05-28 15:49:11 +0300

    Добавлены базовые элементы управления

--- PingPong/PingPong/Form1.Designer.cs
+++ PingPong/PingPong/Form1.Designer.cs
@@ -29,12 +29,54 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.playground = new System.Windows.Forms.Panel();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playground
+            // 
+            this.playground.Controls.Add(this.ball);
+            this.playground.Location = new System.Drawing.Point(12, 12);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(689, 407);
+            this.playground.TabIndex = 0;
+            this.playground.MouseMove += new System.Windows.Forms.MouseEventHandler(this.playground_MouseMove);
+            // 
+            // ball
+            // 
+            this.ball.Location = new System.Drawing.Point(328, 191);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(16, 16);
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(715, 435);
+            this.Controls.Add(this.playground);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.playground.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- PingPong/PingPong/Form1.cs
+++ PingPong/PingPong/Form1.cs
@@ -10,11 +10,34 @@ using System.Windows.Forms;
 
 namespace PingPong
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            timer1.Enabled = true;
+            Cursor.Hide();
+
+        }
+
+        private void playground_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) 
+            { 
+                this.Close(); 
+            } 
         }
     }
 }
