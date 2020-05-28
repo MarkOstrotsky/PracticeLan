269381cccc51dbfa5492738d10600a63750bb862
MarkOstrotsky <MarkOstrotsky@github.com>
2020-05-28 19:29:01 +0300

    Отрисовка ракеток

--- PingPong/PingPong/Form1.Designer.cs
+++ PingPong/PingPong/Form1.Designer.cs
@@ -30,20 +30,48 @@
         {
             this.components = new System.ComponentModel.Container();
             this.playground = new System.Windows.Forms.Panel();
+            this.racketC = new System.Windows.Forms.PictureBox();
+            this.racketP = new System.Windows.Forms.PictureBox();
             this.ball = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.racketC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racketP)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
             this.SuspendLayout();
             // 
             // playground
             // 
+            this.playground.Controls.Add(this.racketC);
+            this.playground.Controls.Add(this.racketP);
             this.playground.Controls.Add(this.ball);
-            this.playground.Location = new System.Drawing.Point(12, 12);
+            this.playground.Location = new System.Drawing.Point(0, 55);
             this.playground.Name = "playground";
-            this.playground.Size = new System.Drawing.Size(689, 407);
+            this.playground.Size = new System.Drawing.Size(724, 407);
             this.playground.TabIndex = 0;
-            this.playground.MouseMove += new System.Windows.Forms.MouseEventHandler(this.playground_MouseMove);
+            // 
+            // racketC
+            // 
+            this.racketC.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.racketC.Location = new System.Drawing.Point(3, 133);
+            this.racketC.Name = "racketC";
+            this.racketC.Size = new System.Drawing.Size(15, 135);
+            this.racketC.TabIndex = 1;
+            this.racketC.TabStop = false;
+            this.racketC.Paint += new System.Windows.Forms.PaintEventHandler(this.racketC_Paint);
+            // 
+            // racketP
+            // 
+            this.racketP.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.racketP.Location = new System.Drawing.Point(676, 133);
+            this.racketP.Name = "racketP";
+            this.racketP.Size = new System.Drawing.Size(15, 135);
+            this.racketP.TabIndex = 1;
+            this.racketP.TabStop = false;
+            this.racketP.Paint += new System.Windows.Forms.PaintEventHandler(this.racketP_Paint);
+            this.racketP.MouseMove += new System.Windows.Forms.MouseEventHandler(this.racketP_MouseMove);
             // 
             // ball
             // 
@@ -57,18 +85,41 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(321, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(379, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(715, 435);
+            this.ClientSize = new System.Drawing.Size(724, 462);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.playground);
             this.Name = "Form1";
             this.Text = "Form1";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.playground.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.racketC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racketP)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -77,6 +128,10 @@
         private System.Windows.Forms.Panel playground;
         private System.Windows.Forms.PictureBox ball;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox racketC;
+        private System.Windows.Forms.PictureBox racketP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- PingPong/PingPong/Form1.cs
+++ PingPong/PingPong/Form1.cs
@@ -13,31 +13,55 @@ namespace PingPong
 
     public partial class Form1 : Form
     {
+        public int speedball = 5;        //Скорость шарика
+        public int pointComp = 0;       //Очки компьютера
+        public int pointPlayer = 0;    //Очки игрока
+       
         public Form1()
         {
             InitializeComponent();
 
             timer1.Enabled = true;
-            Cursor.Hide();
+           // Cursor.Hide();
+            racketP.Left = playground.Right - (playground.Right / 20);
 
         }
 
-        private void playground_MouseMove(object sender, MouseEventArgs e)
-        {
-
-        }
+        
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-
+            //racketP.Top = Cursor.Position.Y - (racketP.Height / 2);
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Escape) 
+            if (e.KeyCode == Keys.Escape)           //Выход на клавишу escape
             { 
                 this.Close(); 
             } 
+        }
+
+        private void racketP_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point pos = e.Location;
+            
+        }
+
+        private void racketP_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            SolidBrush brush = new SolidBrush(System.Drawing.Color.Red);
+            Rectangle rect = new Rectangle(0, 0, 15, 135);
+            e.Graphics.FillRectangle(brush, rect);
+        }
+
+        private void racketC_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            SolidBrush brush = new SolidBrush(System.Drawing.Color.Red);
+            Rectangle rect = new Rectangle(0, 0, 15, 135);
+            e.Graphics.FillRectangle(brush, rect);
         }
     }
 }
